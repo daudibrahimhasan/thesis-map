c093ea53cdf7b822e5cdfdfec34e0256b7b63c46
Daud <daudibrahimhasan@gmail.com>
2026-06-22 02:26:12 +0600

    Exclude backend folder from repository

--- backend/services/index.js
+++
@@ -1,6 +0,0 @@
-const facultyServiceSqlite = require('./faculty-service');
-const facultyServiceSupabase = require('./supabase-service');
-
-const useSupabase = Boolean(process.env.SUPABASE_URL && process.env.SUPABASE_ANON_KEY);
-
-module.exports = useSupabase ? facultyServiceSupabase : facultyServiceSqlite;
--- backend/utils/response.js
+++
@@ -1,5 +0,0 @@
-function createEnvelope(success, data = null, error = null) {
-  return { success, data, error };
-}
-
-module.exports = { createEnvelope };
